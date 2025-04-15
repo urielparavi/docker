2d632e17ff714a6b2222dc40e99d56ef2c913ce9
Your Name <urielparavi75@gmail.com>
2025-04-15 20:51:35 +0300

    210. Starting Project & What We Know Already

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
+const path = require('path');
+const fs = require('fs');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(`
-    <h1>Hello from this NodeJS app!</h1>
-    <p>Try sending a request to /error and see what happens</p>
-  `);
+const filePath = path.join(__dirname, 'story', 'text.txt');
+
+app.use(bodyParser.json());
+
+app.get('/story', (req, res) => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      return res.status(500).json({ message: 'Failed to open file.' });
+    }
+    res.status(200).json({ story: data.toString() });
+  });
 });
 
-app.get('/error', (req, res) => {
-  // This command will stop immediately the node server
-  process.exit(1);
+app.post('/story', (req, res) => {
+  const newText = req.body.text;
+  if (newText.trim().length === 0) {
+    return res.status(422).json({ message: 'Text must not be empty!' });
+  }
+  fs.appendFile(filePath, newText + '\n', (err) => {
+    if (err) {
+      return res.status(500).json({ message: 'Storing the text failed.' });
+    }
+    res.status(201).json({ message: 'Text was stored!' });
+  });
 });
 
-app.listen(8080);
+app.listen(3000);
